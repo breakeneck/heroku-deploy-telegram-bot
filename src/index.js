22997dd51a512a86613487126f983db70c1519d0
Yuri Datsenko <yuri@hosteeva.com>
2017-06-29 11:05:09 +0300

    fixed code structure

--- src/index.js
+++ src/index.js
@@ -42,62 +42,64 @@ bot.onText(/\/start/, (msg, match) => {
 bot.onText(/\/from (.+)/, (msg, match) => {
     let chatId = msg.chat.id;
 
-    if(validateCommand(msg)) {
-        uz.stationSearch(match[1]).then(response => {
-            switch (response.length) {
-                case 0:
-                    bot.sendMessage(msg.chat.id, 'Nothing found, try again please /from command');
-                    break;
-                case 1:
-                    bot.sendMessage(msg.chat.id, 'Departure station is selected, please, search for arrival station using /to command');
-                    chats[chatId].from = match[1];
-                    break;
-                default:
-                    bot.sendMessage(msg.chat.id, 'Selected departure station from list:', {
-                        reply_to_message_id: msg.message_id,
-                        reply_markup: JSON.stringify({
-                            keyboard: response.map((station) => ['/from '+station.title])
-                        })
-                    });
-            }
-        });
-    }
+    if(!validateCommand(msg))
+        return;
+
+    uz.stationSearch(match[1]).then(response => {
+        switch (response.length) {
+            case 0:
+                bot.sendMessage(msg.chat.id, 'Nothing found, try again please /from command');
+                break;
+            case 1:
+                bot.sendMessage(msg.chat.id, 'Departure station is selected, please, search for arrival station using /to command');
+                chats[chatId].from = match[1];
+                break;
+            default:
+                bot.sendMessage(msg.chat.id, 'Selected departure station from list:', {
+                    reply_to_message_id: msg.message_id,
+                    reply_markup: JSON.stringify({
+                        keyboard: response.map((station) => ['/from '+station.title])
+                    })
+                });
+        }
+    });
 });
 
 
 bot.onText(/\/to (.+)/, (msg, match) => {
     let chatId = msg.chat.id;
 
-    if(validateCommand(msg)) {
-        if(!chats[chatId].from)
-            bot.sendMessage(chatId, 'Please, set up from station first using /from command');
+    if(!validateCommand(msg))
+        return;
 
-        uz.stationSearch(match[1]).then(response => {
-            switch (response.length) {
-                case 0:
-                    bot.sendMessage(msg.chat.id, 'Nothing found, try again please /to command');
-                    break;
-                case 1:
-                    bot.sendMessage(msg.chat.id, 'Arrival station is selected, script will check each '+Math.round(scriptRepeatTime/60000) + ' minutes');
-                    chats[chatId].to = match[1];
-                    chats[chatId].lastResponse = '';
+    if(!chats[chatId].from)
+        bot.sendMessage(chatId, 'Please, set up from station first using /from command');
 
-                    // SEARCH FOR RESULT
-                    execUzPing(chatId);
+    uz.stationSearch(match[1]).then(response => {
+        switch (response.length) {
+            case 0:
+                bot.sendMessage(msg.chat.id, 'Nothing found, try again please /to command');
+                break;
+            case 1:
+                bot.sendMessage(msg.chat.id, 'Arrival station is selected, script will check each '+Math.round(scriptRepeatTime/60000) + ' minutes');
+                chats[chatId].to = match[1];
+                chats[chatId].lastResponse = '';
 
-                    // RUN SCHEDULER
-                    chats[chatId].interval = () => setInterval(() => execUzPing(chatId), scriptRepeatTime);
-                    break;
-                default:
-                    bot.sendMessage(msg.chat.id, 'Selected arrival station from list:', {
-                        reply_to_message_id: msg.message_id,
-                        reply_markup: JSON.stringify({
-                            keyboard: response.map((station) => ['/to '+station.title])
-                        })
-                    });
-            }
-        });
-    }
+                // SEARCH FOR RESULT
+                execUzPing(chatId);
+
+                // RUN SCHEDULER
+                chats[chatId].interval = () => setInterval(() => execUzPing(chatId), scriptRepeatTime);
+                break;
+            default:
+                bot.sendMessage(msg.chat.id, 'Selected arrival station from list:', {
+                    reply_to_message_id: msg.message_id,
+                    reply_markup: JSON.stringify({
+                        keyboard: response.map((station) => ['/to '+station.title])
+                    })
+                });
+        }
+    });
 
 });
 
@@ -112,15 +114,16 @@ bot.onText(/\/status/, (msg, match) => {
 bot.onText(/\/stop/, (msg, match) => {
     let chatId = msg.chat.id;
 
-    if(validateCommand(msg)) {
-        // STOP PREVIOUSLY RUNNED SCRIPT
-        if(chats.hasOwnProperty(chatId) && chats[chatId].interval) {
-            clearInterval(chats[chatId].interval);
-            bot.sendMessage(chatId, `Scheduler "${chats[chatId].from} - ${chats[chatId].to}" stopped`);
-        }
-        else
-            bot.sendMessage(chatId, 'Scheduler is not in run state');
+    if(!validateCommand(msg))
+        return;
+
+    // STOP PREVIOUSLY RUNNED SCRIPT
+    if(chats.hasOwnProperty(chatId) && chats[chatId].interval) {
+        clearInterval(chats[chatId].interval);
+        bot.sendMessage(chatId, `Scheduler "${chats[chatId].from} - ${chats[chatId].to}" stopped`);
     }
+    else
+        bot.sendMessage(chatId, 'Scheduler is not in run state');
 });
 
 
