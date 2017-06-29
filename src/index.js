53e3d68b4b6595b0d557feccf8887d1150b8c23f
Yuri Datsenko <yuri@hosteeva.com>
2017-06-29 11:01:49 +0300

    fixed stations list buttons, added stop command, added saving from and to stations at start

--- src/index.js
+++ src/index.js
@@ -24,15 +24,15 @@ bot.onText(/\/start/, (msg, match) => {
     bot.sendMessage(chatId, 'УкрЗалізниця pinger. Use /from command to add departure station');
 
     // STOP PREVIOUSLY RUNNED SCRIPT
-    if(chats.hasOwnProperty(chatId))
+    if(chats.hasOwnProperty(chatId) && chats[chatId].interval)
         clearInterval(chats[chatId].interval);
     
     chats[chatId] = {
         id: chatId,
         interval: null,
         lastResponse: '',
-        from: '',
-        to: '',
+        from: chats[chatId] ? chats[chatId].from : '',
+        to: chats[chatId] ? chats[chatId].to : ''
     };
 });
 
@@ -47,14 +47,14 @@ bot.onText(/\/from (.+)/, (msg, match) => {
                     bot.sendMessage(msg.chat.id, 'Nothing found, try again please /from command');
                     break;
                 case 1:
-                    bot.sendMessage(msg.chat.id, 'Departure station is selected, please, search for departure station /to');
+                    bot.sendMessage(msg.chat.id, 'Departure station is selected, please, search for arrival station using /to command');
                     chats[chatId].from = match[1];
                     break;
                 default:
                     bot.sendMessage(msg.chat.id, 'Selected departure station from list:', {
                         reply_to_message_id: msg.message_id,
                         reply_markup: JSON.stringify({
-                            keyboard: response.map((station) => ['/from '+station])
+                            keyboard: response.map((station) => ['/from '+station.title])
                         })
                     });
             }
@@ -79,15 +79,18 @@ bot.onText(/\/to (.+)/, (msg, match) => {
                     bot.sendMessage(msg.chat.id, 'Arrival station is selected, script will check each '+Math.round(scriptRepeatTime/60000) + ' minutes');
                     chats[chatId].to = match[1];
                     chats[chatId].lastResponse = '';
-                    chats[chatId].interval = () => setInterval(() => execUzPing(chatId), scriptRepeatTime);
 
+                    // SEARCH FOR RESULT
                     execUzPing(chatId);
+
+                    // RUN SCHEDULER
+                    chats[chatId].interval = () => setInterval(() => execUzPing(chatId), scriptRepeatTime);
                     break;
                 default:
                     bot.sendMessage(msg.chat.id, 'Selected arrival station from list:', {
                         reply_to_message_id: msg.message_id,
                         reply_markup: JSON.stringify({
-                            keyboard: response.map((station) => ['/from '+station])
+                            keyboard: response.map((station) => ['/to '+station.title])
                         })
                     });
             }
@@ -100,6 +103,20 @@ bot.onText(/\/to (.+)/, (msg, match) => {
 bot.onText(/\/status/, (msg, match) => {
     if(validateCommand(msg))
         bot.sendMessage(msg.chat.id, chats[chatId].lastResponse);
+});
+
+bot.onText(/\/stop/, (msg, match) => {
+    let chatId = msg.chat.id;
+
+    if(validateCommand(msg)) {
+        // STOP PREVIOUSLY RUNNED SCRIPT
+        if(chats.hasOwnProperty(chatId) && chats[chatId].interval) {
+            clearInterval(chats[chatId].interval);
+            bot.sendMessage(chatId, `Scheduler "${chats[chatId].from} - ${chats[chatId].to}" stopped`);
+        }
+        else
+            bot.sendMessage(chatId, 'Scheduler is not in run state');
+    }
 });
 
 
