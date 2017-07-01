d430a4ba293a90d587bf2eb7a399b23a1091e96a
Yuri Datsenko <yuri@hosteeva.com>
2017-07-01 22:18:25 +0300

    checking autodeploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import helper from './helper';
 import scheduler from './scheduler';
 const fs = require('fs');
 
-const scriptRepeatTime = 0.2*60*1000;
+const scriptRepeatTime = 0.2*60*1000; // yopgogogo
 
 
 console.log('App started');
