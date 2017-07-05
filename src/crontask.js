d06b02ad4967d810870f9fe051fce95334b696ea
Yuri Datsenko <yuri@hosteeva.com>
2017-07-05 22:10:43 +0300

    cron updates

--- src/crontask.js
+++ src/crontask.js
@@ -17,13 +17,12 @@ const LUTSK_ID = 2218060;
 const DNIPRO_ID = 2210700;
 
 
-[
-    {
-        title: 'Луцьк - Дніпро',
-        from: LUTSK_ID,
-        to: DNIPRO_ID,
-        at: '12.07.2017',
-    },{
+[{
+    title: 'Луцьк - Дніпро',
+    from: LUTSK_ID,
+    to: DNIPRO_ID,
+    at: '12.07.2017',
+},{
     title: 'Луцьк - Дніпро',
     from: LUTSK_ID,
     to: DNIPRO_ID,
@@ -38,19 +37,20 @@ const DNIPRO_ID = 2210700;
     from: DNIPRO_ID,
     to: LUTSK_ID,
     at: '17.07.2017',
-}
-].forEach((sched) =>
+}].forEach((sched) => {
+    console.log(`Searching for train ${sched.title} at ${sched.at}`);
+
     uz.searchTrain(sched.from, sched.to, sched.at).then(
         result => (result) => {
-            bot.sendMessage(USER_ID, result);
-            console.log(`Searching for train ${sched.title} at ${sched.at}: ${result}`);
+            bot.sendMessage(USER_ID, `${sched.title} at ${sched.at} ${result}`);
+            console.log(result);
         },
         error => (error) => {
-            bot.sendMessage(USER_ID, error);
-            console.log(`Searching for train ${sched.title} at ${sched.at}: ${error}`);
+            bot.sendMessage(USER_ID, `${sched.title} at ${sched.at} ${result}`);
+            console.log(error);
         }
-    )
-);
+    );
+});
 
 process.exit();
 
